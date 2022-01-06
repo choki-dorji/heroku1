2a90bd1fb3d945d9dc1d69564a090bc477ff4930
choki-dorji <62417033+choki-dorji@users.noreply.github.com>
2022-01-06 14:31:03 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '3gjm^_scxasa&ewe64tb$ass7t)z*a45aycsqnwo3j!+^=xq&j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'localhost']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
